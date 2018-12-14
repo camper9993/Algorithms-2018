4945ad878f8cc42ad93167c35a4b1ce9c11f3ec8
camper9993 <danilov_1999@mail.ru>
2018-12-14 20:45:58 +0300

    the 6th lesson (Тесты добавлю завтра)

--- src/lesson6/DynamicTasks.kt
+++ src/lesson6/DynamicTasks.kt
@@ -2,6 +2,9 @@
 
 package lesson6
 
+import java.io.File
+import java.util.*
+
 /**
  * Наибольшая общая подпоследовательность.
  * Средняя
@@ -29,8 +32,43 @@ fun longestCommonSubSequence(first: String, second: String): String {
  * то вернуть ту, в которой числа расположены раньше (приоритет имеют первые числа).
  * В примере ответами являются 2, 8, 9, 12 или 2, 5, 9, 12 -- выбираем первую из них.
  */
+// Трудоемкость : N(n*log(n)) (алгоритм взят с wiki)
+// Ресурсоемкость : R(n)
 fun longestIncreasingSubSequence(list: List<Int>): List<Int> {
-    TODO()
+    val result: MutableList<Int> = mutableListOf()
+    if (list.isEmpty()) return result
+    val size = list.size
+    val arrayLength: Array<Int?> = Array(size) { -1 }
+    val arrayPath: Array<Int?> = Array(size) { -1 }
+    var max: Int?
+    var jMax: Int?
+    for (i in 0 until size) {
+        max = 0
+        jMax = 0
+        for (j in 0 until i) {
+            if (list[j] < list[i] && arrayLength[j]!! > max!!) {
+                max = arrayLength[j]
+                jMax = j
+            }
+        }
+        arrayLength[i] = max!! + 1
+        arrayPath[i] = jMax
+    }
+    var n = 0
+    for (k in 0 until size) {
+        if (arrayLength[k]!! > arrayLength[n]!!) n = k
+    }
+    while (true) {
+        if (n == 0) {
+            result.add(list[n])
+            break
+        }
+        result.add(list[n])
+        n = arrayPath[n]!!
+    }
+
+    result.reverse()
+    return result
 }
 
 /**
@@ -53,9 +91,31 @@ fun longestIncreasingSubSequence(list: List<Int>): List<Int> {
  *
  * Здесь ответ 2 + 3 + 4 + 1 + 2 = 12
  */
+//Трудоемскость : N(height * width)
+//Ресурсоемкость : R(height * width)
 fun shortestPathOnField(inputName: String): Int {
-    TODO()
+    val input = Scanner(File(inputName))
+    val table: MutableList<Array<Int>> = mutableListOf()
+    while (input.hasNextLine())
+        table.add(input.nextLine().split(" ").map { it.toInt() }.toTypedArray())
+    input.close()
+
+    val width = table[0].size
+    val height = table.size
+
+    for (i in 0 until height)
+        for (j in 0 until width) {
+            table[i][j] = getBestMove(table, i, j) + table[i][j]
+        }
+
+    return table[height - 1][width - 1]
 }
 
+fun getBestMove(table: MutableList<Array<Int>>, i: Int, j: Int): Int {
+    return if (i == 0 && j == 0) 0
+    else if (i == 0) table[i][j - 1]
+    else if (j == 0) table[i - 1][j]
+    else Math.min(Math.min(table[i - 1][j - 1], table[i][j - 1]), table[i - 1][j])
+}
 // Задачу "Максимальное независимое множество вершин в графе без циклов"
 // смотрите в уроке 5
